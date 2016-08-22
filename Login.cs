12550806c0cee24584a3d9ec35082fb336af3d53
AP <ap>
2016-08-21 23:31:31 -0600

    Login window

---
+++ Login.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+            textBox1.Focus();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            attemptLogin();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                attemptLogin();
+            }
+            else if (e.KeyCode == Keys.Escape)
+            {
+                this.escape();
+            }
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                attemptLogin();
+            }
+            else if (e.KeyCode == Keys.Escape)
+            {
+                this.escape();
+            }
+        }
+
+        private void button1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                attemptLogin();
+            }
+            else if (e.KeyCode == Keys.Escape)
+            {
+                this.escape();
+            }
+        }
+
+        private void attemptLogin()
+        {
+            MessageBox.Show("Login attempt!");
+        }
+
+        private void escape()
+        {
+            this.Dispose();
+        }
+    }
+}
